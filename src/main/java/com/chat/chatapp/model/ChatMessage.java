f470a4e91f14e544a69892210f5a00410b5fcd7d
siddharth-2408 <gsiddharth2201@email.com>
2025-06-29 13:09:23 +0530

    Initial Commit

---
+++ src/main/java/com/chat/chatapp/model/ChatMessage.java
@@ -0,0 +1,13 @@
+package com.chat.chatapp.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+    private String time;
+}
